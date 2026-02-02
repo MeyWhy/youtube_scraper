6bbbf5f067af017a5b85bde65781e0dcac45e3de
MeyWhy <104941102+MeyWhy@users.noreply.github.com>
2026-02-02 02:07:48 +0100

    Update youtube_scraper.py

--- youtube_scraper.py
+++ youtube_scraper.py
@@ -1,5 +1,7 @@
+import json
 import os
 import pickle
+import csv
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
@@ -33,56 +35,106 @@ if not credentials or not credentials.valid :
 
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
-
-#API CALLS PART 1: go over the init playlist
 vids_list=[]
+categories_dict={}
+
+#retrieve video ids from wl playlist
+with open('Vidéos de Watch later.csv', 'r', encoding='utf-8-sig') as f:
+    reader=csv.DictReader(f, delimiter=';')
+    for row in reader:
+        vids_list.append(row['videoId'])
+
+#retrieve category ids
+with open('categories.json', 'r', encoding='utf-8')as f:
+    data=json.load(f)
+for item in data["items"]:
+    categories_dict[item["id"]]= item["snippet"]["title"]
+    
+
+#API CALLS PART 1: get info about all vids
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials= credentials)
-def playlist_browse():
-  search_response = youtube.playlistItems().list(
-    part='status',
-    maxResults=10,
-    playlistId='xxx'
-  ).execute()
-  items=search_response['items']
-  for r in items:
-    vids_list.append(r['snippet']['resourceId']['videoId'])
+def playlist_browse():      
+    batch_size=50
+    vids=[]
+    for i in range(0, len(vids_list), batch_size):
+        batch_ids=vids_list[i:i+batch_size]
+        if not batch_ids:
+            continue
+        search_response = youtube.videos().list(
+            part='snippet, topicDetails, contentDetails',
+            id=','.join(batch_ids)
+        ).execute()
 
-  return items
+        details=search_response.get('items', [])
+       
+        for d in details: 
+            vid_cat=d['snippet']['categoryId']
+            vid_id=d['id']
+            vids.append((vid_id, vid_cat))
 
-#playlist_browse()
+    return vids
+
+def video_category_correspondance(vid_id, vid_cat):
+    category_name=categories_dict.get(vid_cat)
+    return (vid_id, category_name)
 
 #API CALL PART 2: create the new playlists
-def playlist_create():
+def playlist_create(category_name):
     create_response = youtube.playlists().insert(
         part='snippet, status',
         body={'snippet':{
-                'title': 'Test',
-                'description': 'test the description',
+                'title': category_name, 
+                'description': f'description of playlist for {category_name}', 
                 },
               'status':{
                   'privacyStatus': 'private',
               }
             }
         ).execute()
-    print(create_response)
-#playlist_create()
+    return create_response['id']
+
 
 #API CALL PART 3: insert the videos in the correct playlists
-def playlist_add():
+def playlist_add(playlist_id, video_id):
     add_response = youtube.playlistItems().insert(
         part='snippet',
         body={'snippet':{
-                'playlistId': 'xxx',
+                'playlistId': playlist_id,
                 'resourceId': {
                     'kind': 'youtube#video',
-                    'videoId': 'xxx'
+                    'videoId': video_id 
                 }
         }}
     ).execute()
-    print(add_response)
-#playlist_add()
-#CLASSIFICATION PART
-categories = ["litterature", "network", "cinema", "music", "philosophy",
-              "commentary", "lord of the ring", "diy", "tips", "politics",
-              "economy", "computer science", "physics", "mathematics", "makeup",
-              "vlog", "storytime", "cooking", "programming", "conferences"]
+    return add_response
+
+#Call for all the funcs
+video_corresponding=[video_category_correspondance(vid_id, vid_cat)
+              for vid_id, vid_cat in playlist_browse()]
+
+playlist_ids={}
+for _, cat_name in video_corresponding:
+    if cat_name is None:
+        print("skip. unknown category for the video")
+        continue
+    if cat_name not in playlist_ids:
+        playlist_ids[cat_name]=playlist_create(category_name=cat_name)
+
+
+with open('Vidéos de Watch later.csv', 'r', encoding='utf-8-sig') as f:
+    reader= csv.DictReader(f, delimiter=';')
+    rows=list(reader)
+
+for vid_id, cat_name in video_corresponding:
+    playlist_id = playlist_ids.get(cat_name)
+    if playlist_id:
+        playlist_add(playlist_id=playlist_id, video_id=vid_id)
+        
+        rows=[row for row in rows if row['videoId']!=vid_id]
+
+        with open('Vidéos de Watch later.csv', 'w', encoding='utf-8-sig', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=reader.fieldnames, delimiter=';')
+            writer.writeheader()
+            writer.writerows(rows)
+
+print("all videos processed and removed from csv")
